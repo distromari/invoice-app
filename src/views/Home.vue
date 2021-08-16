a8636dd59e808212d86ed5d5504eb742c5f689c1
distromari <moraismariana83@gmail.com>
2021-08-15 21:27:33 -0300

    Creating header component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="Home"></div>
+  <div class="home container">
+    <!-- Header -->
+    <div class="header flex">
+      <div class="left flex flex-column">
+        <h1>Invoices</h1>
+        <span>There are 4 total invoices</span>
+      </div>
+      <div class="right flex">
+        <div @click="toggleFilterMenu" class="filter flex" ref="filter">
+          <span>Filter by status</span>
+          <img src="@/assets/svg/icon-arrow-down.svg" alt="">
+          <ul v-show="filterMenu" class="filter-menu">
+            <li>Draft</li>
+            <li>Pending</li>
+            <li>Paid</li>
+            <li>Clear filter</li>
+          </ul>
+        </div>
+        <div @click="newInvoice" class="button flex">
+          <div class="inner-button flex">
+            <img src="@/assets/svg/icon-plus.svg" alt="">
+          </div>
+          <span>New invoice</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
